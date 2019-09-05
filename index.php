f886a271499cb713c91c6c9fb409c9e57949f7df
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-09-05 22:30:11 +0400

    fix

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 use infrajs\load\Load;
 use infrajs\ans\Ans;
 use infrajs\config\Config;
+use infrajs\template\Template;
+use infrajs\controller\Crumb;
+use infrajs\layer\seojson\Seojson;
 
 
 $path = Ans::GET('path','string','');
@@ -10,17 +13,47 @@ $path = '/'.$path;
 $conf = Config::get('seo');
 $data = Load::loadJSON('-excel/get/group/SEO/?src='.$conf['src']);
 $list = [];
+
+
+Crumb::change($path);
+$crumb = Crumb::getInstance();
+$layer = array('crumb' => $crumb);
+
 if (!empty($data['data']['data'])) {
-	foreach ($data['data']['data'] as $v) {
-		if(empty($v['Адрес'])) continue;
-		$list[$v['Адрес']] = $v;
-		if ($conf['common']) $list[$v['Адрес']]['external'] = $conf['common'];
-		unset($list[$v['Адрес']]['Адрес']);
+	foreach ($data['data']['data'] as $item) {
+		if (empty($item['Адрес'])) continue;
+		$adr = Template::parse([$item['Адрес']], $layer);		
+		if (isset($list[$adr])) continue;
+		$list[$adr] = $item;
+		if ($conf['common']) $list[$adr]['external'] = $conf['common'];
+		//unset($list[$adr]['Адрес']);
 	}
 }
 
 if (isset($list[$path])) {
-	return Ans::ans($list[$path]);
+
+	$item = $list[$path];
+	if (!empty($item['jsontpl'])) {
+		$item['json'] = Template::parse([$item['jsontpl']], $layer);
+	}
+
+	if (!empty($item['json'])) {
+		$item['data'] = Load::loadJSON($item['json']);
+		if (empty($item['dataroot'])) $item['dataroot'] = null;
+		foreach(['description','keywords','title','image_src'] as $prop) {
+			if (empty($item[$prop])) continue;
+			$item[$prop] = Template::parse([$item[$prop]], $item['data'], null, $item['dataroot']);	
+		}
+		if (!empty($item['image_src'])) $item['image_src'] = Seojson::getSite().'/'.$item['image_src'];
+	}
+	//echo '<pre>';
+	//print_r($item);
+	//exit;
+	return Ans::ans($item);
+	
+	
+
+	
 } else {
 	if (!isset($list['/'])) {
 		$list['/'] = array('auto' => true);
